7c77c6ad19bb8610d96488f4f9890bfc0830dc6d
Settawut_tt.github.io <71252576+Settawut-tt@users.noreply.github.com>
2022-03-18 15:10:13 +0700

    client ID

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,5 +1,5 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
-const clientId = 'YOUR SPOTIFY CLIENT ID';
+const clientId = '49bbf5fab82a422c9c20b63776b68de6';
 
 const redirectUri = window.location.href;
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
